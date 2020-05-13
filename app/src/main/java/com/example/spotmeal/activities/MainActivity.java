47048ba076733075540369bc7368cc53b8c491f1
sevcukd <44055621+sevcukd@users.noreply.github.com>
2020-05-13 22:04:17 +0300

    add Button

--- app/src/main/java/com/example/spotmeal/activities/MainActivity.java
+++ app/src/main/java/com/example/spotmeal/activities/MainActivity.java
@@ -23,12 +23,14 @@ public class MainActivity extends AppCompatActivity {
         if(mData.contains(getString(R.string.APP_PREFERENCES_NAME))){
             Toast.makeText(getApplicationContext(),"Ви авторизувались",Toast.LENGTH_LONG).show();
             Intent intent = new Intent(MainActivity.this, SignUp.class);
-            try {
-                TimeUnit.SECONDS.sleep(3);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            Intent intent2 = new Intent(MainActivity.this, LogIn.class);
+//            try {
+//                Thread.sleep(5000); //Приостанавливает поток на 1 секунду
+//            } catch (Exception e) {
+//
+//            }
             startActivity(intent);
+//            startActivity(intent2);
         }else{
             Intent intent = new Intent(MainActivity.this, LogIn.class);
             startActivity(intent);
